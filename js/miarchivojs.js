e8dedcd1eb7df7466cf5a4ccc317a5a443729909
iruleicach <iru.leicach@gmail.com>
2022-08-10 20:36:47 -0300

    Modificando la funcionalidad de la heladería. Sumando Arrays

--- js/miarchivojs.js
+++ js/miarchivojs.js
@@ -1,4 +1,4 @@
-//Simulador interactivo
+//Simulador interactivo: heladería
 
 //1)Usuarix ingresa su nombre 
 let nombreConsumidor = prompt("Bienvenidx! Por favor ingresá tu nombre")
@@ -9,52 +9,120 @@ let precioMedio = 500;
 let precioKilo = 1000;
 let saldoDeudor = 0;
 
-/*)Creo función donde usuarix ingresa cantidad deseada 
-(en caso de elegir un tamaño de helado inválido, debe volver a ingresar)*/
+//3)Creo un array que contiene los sabores y sus descripciones
+let saboresDescripcion = [
+    {gusto: "Chocolate del alpi",
+    descripcion: "Chocolate 100% cacao con pedacitos de choco blanco"},
+    {gusto: "Crema de la casa",
+    descripcion: "Chantilly con licor"},
+    {gusto: "Vainilla",
+    descripcion: "Receta original de vanilla con nueces"},
+    {gusto: "Suspiro de ddl",
+    descripcion: "Mousse de dulce de lehce con galletitas"},
+    {gusto: "KIDS",
+    descripcion: "Crema americana con rocklets"},
+    {gusto: "Frambuesa granizada",
+    descripcion: "Frambuesa al agua con pedazos de chocolate"},
+];
 
-let eleccion = '';
-    do {
-        let pedido = prompt("Hola " + nombreConsumidor + " cuántos kilos de helado querés entre: 1/2 , 1/4 o 1?")
-        
-        switch (pedido) {
-            case "1/2":
-                eleccion = "1/2"
-                alert("El medio kilo te sale $" + precioMedio)      
-                break;
-            case "1/4":
-                eleccion = "1/4"
-                alert("El cuarto kilo te sale $" + precioCuarto)  
-                break;
-            case "1":
-                eleccion = "1"
-                alert("El kilo te sale $" + precioKilo)
-                break;
-            default:
-                alert("Estás ingresando una cantidad incorrecta. Debés elegir entre 1/2, 1/4 y 1")
-                break;
-            } 
+//Creo la funcionalidad de mi heladería el usuarix puede ver gustos - agregar gustos - comprar helado - salir
+
+let heladeria = ""
+
+while (heladeria != "SALIR"){
+    heladeria = prompt("Hola " + nombreConsumidor + " ingresá qué preferis entre: VER AGREGAR COMPRAR O SALIR")
+    switch (heladeria){
+        case "VER":
+            verSabores()
+            break;
+        case "COMPRAR":
+            eleccionTamanio();
+            break;
+        case "AGREGAR":
+            agregarHelado();
+            break;
+        case "SALIR":
+            alert("Gracias por visitarnos!")
+            break;
+        default:
+            alert("Ingrese una acción válida")
+            break;
+    }
+}
+
+//4) El usuarix puede elegir ver o no los sabores de la heladería
+function verSabores(){
+        saboresDescripcion.forEach(sabor => {
+        alert(sabor.gusto + "  -  Descripción del gusto: " + sabor.descripcion)
+        });
+}
 
 
-    }while (eleccion == '');
-    
-  
-//4)Calculo cuánto vuelto se le dará en función de su compra
-let calcularVuelto = () => {
-        if(eleccion == "1"){
-            saldoDeudor = precioKilo
-        }else if(eleccion == "1/4"){
-            saldoDeudor = precioCuarto
-        }else{
-            saldoDeudor = precioMedio
-        }
-        
-        let abono = prompt("Con cuánto deseás abonar?");
-        if (abono < saldoDeudor) {
-            alert("No te alcanza el dinero")
-        } else {
-            let vuelto = (abono - saldoDeudor);
-            alert("Tu vuelto es: " + vuelto);
-        }
+//5) Creo función donde usuarix ingresa cantidad deseada
+function eleccionTamanio(){
+
+    let pedido = prompt("Hola " + nombreConsumidor + " cuántos kilos de helado querés entre: 1/2 , 1/4 o 1?")
+
+    if(pedido == "1/2"){
+        alert("El medio kilo te sale $" + precioMedio)
+        alert("Muchas gracias por tu compra")    
+    }else if(pedido == "1/4"){
+        alert("El cuarto kilo te sale $" + precioCuarto)
+        alert("Muchas gracias por tu compra")       
+    }else if(pedido == "1"){
+        alert("El kilo te sale $" + precioKilo)
+        alert("Muchas gracias por tu compra")      
+    }else{
+        alert("Estás ingresando una cantidad incorrecta. Por favor volvé a elegir una opción")
     } 
 
-    calcularVuelto(); 
+}
+
+
+/*//7) Creo la función agregarHelado() 
+- Guardo el gusto y descripción del helado del usuario. 
+- Corroboro si el gusto existe entre mis sabores
+- Creo la funcion verSaboresActualizados() para que el usuarix pueda ver cómo se agregó su sabor
+*/
+
+function agregarHelado() {
+            
+    let saborNuevo = prompt("Elegí un nombre para tu nuevo sabor");
+    let descripcionNueva = prompt("Describí brevemente el sabor");
+
+    let corroboroGusto = saboresDescripcion.some((helado)=>{
+        return helado.gusto == saborNuevo
+    });
+
+
+    if(corroboroGusto){
+        alert("Ese gusto ya lo tenemos :)")
+    } else {                
+        const saborUsuarix = {
+            gusto: saborNuevo,
+            descripcion: descripcionNueva,
+        }
+
+        function verSaboresActualizados(){
+            let verGustoNuevo = prompt("Querés ver si tu gusto fue correctamente agregado? responde SI o NO");
+            if(verGustoNuevo == "SI"){
+            verSabores();
+            }else{
+            alert("Hasta la próxima visita!")
+            };
+        }
+
+        saboresDescripcion.push(saborUsuarix); 
+        verSaboresActualizados()
+    }
+}
+
+
+/*     let incluirGusto = prompt("Querés agregar un sabor a la heladaría? Responde SI o NO")
+    if (incluirGusto == "SI"){
+       agregarHelado();
+    }else if(incluirGusto == "NO"){
+        alert("Qué pena! Hasta la próxima visita!")
+    }else{
+        alert("no pudimos entenderte! Hasta la próxima visita!")
+    } */
